94ea5a776ff443757cb68ebf8688e63509e3356b
Leonardo Davanço <leonardohdav@gmail.com>
2022-04-05 13:56:30 -0300

    primeiro commit

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {User} from '@firebase/auth-types';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +9,18 @@ export class AuthService {
 
   constructor(private afAuth: AngularFireAuth) { }
 
-
- /*
-    private signInWithEmail({ email, password }): Promise<UserCredential> {
-    return this.afAuth.signInWithEmailAndPassword(email, password);
+    private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
   }
 
-  private signUpWithEmail({ email, password }): Promise<auth.UserCredential>{
-    return this.afAuth
-    .createUserWithEmailAndPassword(email, password);
-    .then(creeadentials =>
+  private signUpWithEmail({ email, password, name }): Promise<auth.UserCredential>{
+    return this.afAuth.auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(credentials =>
       credentials.user
-        .updateProfile({displayName: name, photoUrl:null });
+        .updateProfile({displayName : name, photoURL : null })
         .then(() => credentials)
     );
   }
-  */
+
 }
